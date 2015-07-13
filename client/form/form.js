f64b480c340cf418ef54ac7c11d8be6984890e05
voxlol <voxlol@outlook.com>
2015-07-13 16:43:00 -0500

    Bug fixes, working on moment.js integration

--- client/form/form.js
+++ client/form/form.js
@@ -2,30 +2,41 @@ Template.form.events({
   'submit #symbolform' : function(e){
     e.preventDefault();
     var symbol = $(e.target).find('[name=symbol]').val()
+
+    // Validation
     $(e.target).find('[name=symbol]').val('')
 
     Session.set('symbol', symbol);
 
     // meteor server-side method call
     Meteor.call('fetchHeadlines', symbol, function(err, result){
+      if(err){
+        console.log(err);
+        return;
+      }
+
       Session.set('symbolDescription',result.description);
 
       var targetCollection = Headlines.findOne({symbol: symbol});
 
       if(!targetCollection){
         // Create a new collection for the symbol
-        console.log('not found for ' + symbol);
-        Headlines.insert({
-          symbol: symbol,
-          description : result.description,
-          items : result.items,
-        });
+        if(result.description === "Yahoo! Finance: RSS feed not found"){
+          return;
+        }else{
+          console.log('not found for ' + symbol);
+          Headlines.insert({
+            symbol: symbol,
+            description : result.description,
+            items : result.items,
+          });
+        }
       }
       else{
         // Update existing collection's items
         console.log('found collection for ' + symbol)
         var id = targetCollection._id;
-        Headlines.insert({_id: id}, {$set: {items : result.items}})
+        Headlines.update({_id: id}, {$set: {items : result.items}})
       }
 
     });
--- client/headlines/headlines.js
+++ client/headlines/headlines.js
@@ -1,6 +1,12 @@
 Template.headlines.helpers({
   'headlines' : function(){
     var symbol = Session.get('symbol') ? Session.get('symbol') : 'SPY';
-    return Headlines.findOne({symbol:symbol}).items;
+    var currentHeadline = Headlines.findOne({symbol:symbol});
+    return currentHeadline ? currentHeadline.items : null;
+  },
+
+  'description' : function(){
+    var description = Session.get('symbolDescription');
+    return description ? description : "Latest Financial News for SPDR S&P 500";
   }
 })
